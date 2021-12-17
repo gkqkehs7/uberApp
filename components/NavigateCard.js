b2599a92f4f868eb34f697502ba828f3ff465a75
minu <gkqkehs10@naver.com>
2021-12-17 23:05:33 +0900

    apply keyboardAvoidingView on App.js

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -6,6 +6,7 @@ import { GOOGLE_MAPS_APIKEY } from "@env"
 import { useDispatch } from 'react-redux'
 import { setDestination } from '../slices/navSlice'
 import { useNavigation } from '@react-navigation/native'
+import NavFavourites from './NavFavourites'
 
 const NavigateCard = () => {
     const dispatch = useDispatch();
@@ -38,6 +39,8 @@ const NavigateCard = () => {
                         debounce={400}
                     />
                 </View>
+
+                <NavFavourites />
             </View>
         </SafeAreaView>
     )
