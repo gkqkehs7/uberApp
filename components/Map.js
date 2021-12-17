c20996197455d8ec58a66d9c0d546aca125cada1
minu <gkqkehs10@naver.com>
2021-12-17 22:46:21 +0900

    zooming map using useRef, fitToSuppliedMarkers

--- components/Map.js
+++ components/Map.js
@@ -17,10 +17,11 @@ const Map = () => {
         return ;
       }
 
-      mapRef.current.fitTouppliedMarkers(['origin','destination'], {
+      mapRef.current.fitToSuppliedMarkers(['origin','destination'], {
         edgePadding: { top: 50, right:50, bottom:50, left:50 }
       })
     }, [origin, destination])
+
     return (
         <MapView
         ref={mapRef}
